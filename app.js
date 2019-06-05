0a964c81cfb30d089f8ae82efa3c5daa36846c1d
Yuriy <yuriy@gmail.com>
2019-06-05 22:39:53 +0300

    test commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
-
+const appDiv = document.getElementById('app');
 const tagP = document.createElement('p');
+appDiv.appendChild(tagP);
 tagP.innerHTML = '<strong>HELLO!</strong>';
-console.log(tagP);
+//console.log(tagP);
+
